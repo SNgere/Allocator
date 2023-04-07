e129f01085bc6be6b383c7f48ca6f2427e793aad
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-07 20:03:06 +0300

    Update job.py

--- job.py
+++ job.py
@@ -14,15 +14,27 @@ df2.set_index('Date', inplace=True)
 # Filter the dataframe to only show Monday to Friday
 df2_weekdays = df2.loc[df2.index.weekday < 5]
 
-# Define a function to format the date as "Monday, DD/MM/YYYY" and color every Friday in maroon
-def format_date_and_color_friday(date):
-    if date.weekday() == 4:  # Friday
-        return ['background-color: maroon; color: white']*len(date), date.strftime('%A, %d/%m/%Y')
-    else:
-        return ['']*len(date), date.strftime('%A, %d/%m/%Y')
+# Define a function to format the date as "Monday, DD/MM/YYYY" and highlight every Friday in maroon
+def format_date_and_highlight_fridays(date):
+    if date.weekday() == 4:
+        return [f'background-color: #800000; color: white'] * len(date)
+    return [''] * len(date)
 
-# Apply the date format and color to the index column
-df2_weekdays_styled = df2_weekdays.style.apply(lambda x: format_date_and_color_friday(x.index))
+df2_weekdays_styled = df2_weekdays.style.apply(format_date_and_highlight_fridays, axis=1)
+
+# Apply the date format to the index column
+df2_weekdays_styled.set_caption('Weekly Data')
+df2_weekdays_styled.set_table_styles(
+    [{
+        'selector': 'caption',
+        'props': [('color', '#008080'),
+                  ('font-size', '18px'),
+                  ('text-align', 'center'),
+                  ('font-weight', 'bold'),
+                  ('padding-top', '12px'),
+                  ('padding-bottom', '12px')]
+    }]
+)
 
 # Display the styled dataframe in Streamlit
 st.write(df2_weekdays_styled)
