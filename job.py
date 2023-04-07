e07ea955e15d212e05603100ec12a2565a31f480
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-08 00:41:21 +0300

    Update job.py

--- job.py
+++ job.py
@@ -143,10 +143,10 @@ fig, ax = plt.subplots(figsize=(1,1))
 ax.pie(df['Count'], labels=df['Color'], autopct='%1.1f%%', startangle=90,
        colors=colors, explode=explode, shadow=True)
 ax.axis('equal')
-ax.set_title('Cell Counts by Fill Color', fontweight='bold')
+#ax.set_title('Cell Counts by Fill Color', fontweight='bold')
 
 # Add legend
-legend = ax.legend(loc='best', bbox_to_anchor=(1, 0.5))
+#legend = ax.legend(loc='best', bbox_to_anchor=(1, 0.5))
 
 # Show the pie chart in Streamlit
 st.pyplot(fig)
