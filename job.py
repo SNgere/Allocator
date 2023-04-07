ae2a35c1e76639b22d02c62f734c79c35f76f86d
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-07 17:04:10 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-# Load the Excel file into a Pandas DataFrame
-excel_file = pd.read_excel('https://github.com/SNgere/Allocator/blob/e55257a9658e72e3093c18d212afbee8df88ad2d/job.xlsx', sheet_name=None, engine='openpyxl')
-
-# Get the DataFrame for each sheet
-sheet1_df = excel_file['Sheet1']
-sheet2_df = excel_file['Sheet2']
+# Load the CSV files into Pandas DataFrames
+staff_df = pd.read_csv('https://github.com/SNgere/Allocator/blob/3129b1761d8c113ac7a4fe941bd45356ad64c972/staff.csv')
+work_df = pd.read_csv('https://github.com/SNgere/Allocator/blob/3129b1761d8c113ac7a4fe941bd45356ad64c972/job.csv')
 
 # Define the Streamlit app
 def app():
@@ -14,14 +11,14 @@ def app():
     st.title("Work Allocation")
 
     # Create a dropdown to select the staff member
-    staff_names = sheet1_df['Staff'].tolist()
+    staff_names = staff_df['Staff'].tolist()
     selected_staff = st.selectbox("Select Staff Member", staff_names)
 
     # Create a date input to select the week
     selected_date = st.date_input("Select Week Starting")
 
     # Get the work allocation for the selected staff member and week
-    work_allocation = sheet2_df.loc[(sheet2_df['Date'] == selected_date) & (sheet2_df[selected_staff].notnull())]
+    work_allocation = work_df.loc[(work_df['Date'] == selected_date) & (work_df[selected_staff].notnull())]
     
     # If work allocation is found, display it in a table
     if not work_allocation.empty:
