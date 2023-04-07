acb7576a0306f1f404dbc6c34d0513bf98d7b835
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-07 18:14:36 +0300

    Update job.py

--- job.py
+++ job.py
@@ -13,21 +13,6 @@ df = pd.read_csv("https://raw.githubusercontent.com/SNgere/Allocator/main/job.cs
 
 # Convert the "Date" column to datetime format
 df['Date'] = pd.to_datetime(df['Date'])
-import streamlit as st
-import pandas as pd
-
-# Set page title
-st.set_page_config(page_title="Weekly Work Allocation", page_icon=":memo:", layout="wide")
-
-# Set page header
-st.title("Weekly Work Allocation")
-st.markdown("This table shows the weekly work allocation for each team member.")
-
-# Load the data
-df = pd.read_csv("https://raw.githubusercontent.com/SNgere/Allocator/main/job.csv")
-
-# Convert the "Date" column to datetime format
-df['Date'] = pd.to_datetime(df['Date'])
 
 # Group the data by week and sum the values in each group
 weekly_work = df.groupby(pd.Grouper(key='Date', freq='W-MON')).sum()
@@ -44,19 +29,3 @@ filtered_work = weekly_work.loc[start_date:end_date]
 
 # Display the resulting dataframe with formatting
 st.write(filtered_work.style.set_caption("Work allocation from April 10th to April 14th").format("{:.0f}").set_properties(**{'text-align': 'center'}))
-
-# Group the data by week and sum the values in each group
-weekly_work = df.groupby(pd.Grouper(key='Date', freq='W-MON')).sum()
-
-# Drop any NaN values in the resulting dataframe
-weekly_work = weekly_work.dropna(how='all')
-
-# Define the start and end date for the date range filter
-start_date = pd.to_datetime('2023-04-10')
-end_date = pd.to_datetime('2023-04-14')
-
-# Filter the data by date range
-filtered_work = weekly_work.loc[start_date:end_date]
-
-# Display the resulting dataframe with formatting
-st.write(filtered_work.style.set_caption("Work allocation from April 10th to April 14th").format("{:.0f}").set_properties(**{'text-align': 'center', 'font-size': '18px'}))
