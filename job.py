c18e94ee29a306197b1b4090e95ae172c706ceec
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-07 17:40:15 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,29 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-# Load the CSV files into Pandas DataFrames
-staff_df = pd.read_csv('https://github.com/SNgere/Allocator/blob/3129b1761d8c113ac7a4fe941bd45356ad64c972/staff.csv')
-work_df = pd.read_csv('https://github.com/SNgere/Allocator/blob/3129b1761d8c113ac7a4fe941bd45356ad64c972/job.csv')
+# Load the data
+df = pd.read_csv("https://github.com/SNgere/Allocator/blob/ae2a35c1e76639b22d02c62f734c79c35f76f86d/job.csv")
 
-# Define the Streamlit app
-def app():
-    # Add a title to the app
-    st.title("Work Allocation")
+# Convert the "Date" column to datetime format
+df['Date'] = pd.to_datetime(df['Date'])
 
-    # Create a dropdown to select the staff member
-    staff_names = staff_df['Staff'].tolist()
-    selected_staff = st.selectbox("Select Staff Member", staff_names)
+# Group the data by week and sum the values in each group
+weekly_work = df.groupby(pd.Grouper(key='Date', freq='W-MON')).sum()
 
-    # Create a date input to select the week
-    selected_date = st.date_input("Select Week Starting")
+# Drop any NaN values in the resulting dataframe
+weekly_work = weekly_work.dropna(how='all')
 
-    # Get the work allocation for the selected staff member and week
-    work_allocation = work_df.loc[(work_df['Date'] == selected_date) & (work_df[selected_staff].notnull())]
-    
-    # If work allocation is found, display it in a table
-    if not work_allocation.empty:
-        st.write(f"Work for {selected_staff} in week starting {selected_date}:")
-        st.dataframe(work_allocation, index=False)
-    # If no work allocation is found, display a message
-    else:
-        st.write(f"No work found for {selected_staff} in week starting {selected_date}.")
+# Define the start and end date for the date range filter
+start_date = pd.to_datetime('2023-04-10')
+end_date = pd.to_datetime('2023-04-14')
+
+# Filter the data by date range
+filtered_work = weekly_work.loc[start_date:end_date]
+
+# Display the resulting dataframe
+st.write(filtered_work)
