cc2cf1b0aa178b50df5a127445a73dd038e4059b
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-07 17:59:26 +0300

    Update job.py

--- job.py
+++ job.py
@@ -22,20 +22,21 @@ weekly_work = weekly_work.dropna(how='all')
 
 # Define the start and end date for the date range filter
 start_date = pd.to_datetime('2023-04-10')
-end_date = pd.to_datetime('2023-04-13')
+end_date = pd.to_datetime('2023-04-14')
 
 # Filter the data by date range
 filtered_work = weekly_work.loc[start_date:end_date]
 
 # Define the color map for each name
-cmap = {'Alice': 'lightblue', 'Bob': 'lightgreen', 'Charlie': 'lightpink', 'Dave': 'lavender', 'Eve': 'lightyellow'}
+cmap = {'Daemon': 'lightblue', 'Emma': 'lightgreen', 'Freya': 'lightpink', 'Georgie': 'lavender', 'Jimmy': 'lightyellow','Jones': 'lightyellow'}
 
 # Apply background gradient to each name based on its color map
-styled_table = filtered_work.style.background_gradient(subset=pd.IndexSlice[:, 'Alice'], cmap=cmap['Alice']).\
-              background_gradient(subset=pd.IndexSlice[:, 'Bob'], cmap=cmap['Bob']).\
-              background_gradient(subset=pd.IndexSlice[:, 'Charlie'], cmap=cmap['Charlie']).\
-              background_gradient(subset=pd.IndexSlice[:, 'Dave'], cmap=cmap['Dave']).\
-              background_gradient(subset=pd.IndexSlice[:, 'Eve'], cmap=cmap['Eve'])
+styled_table = filtered_work.style.background_gradient(subset=pd.IndexSlice[:, 'Daemon'], cmap=cmap['Daemon']).\
+              background_gradient(subset=pd.IndexSlice[:, 'Emma'], cmap=cmap['Emma']).\
+              background_gradient(subset=pd.IndexSlice[:, 'Freya'], cmap=cmap['Freya']).\
+              background_gradient(subset=pd.IndexSlice[:, 'Georgie'], cmap=cmap['Georgie']).\
+              background_gradient(subset=pd.IndexSlice[:, 'Jimmy'], cmap=cmap['Jimmy'])
+              background_gradient(subset=pd.IndexSlice[:, 'Jones'], cmap=cmap['Jones'])
 
 # Display the resulting dataframe with formatting and color map
 st.write(styled_table.set_caption("Work allocation from April 10th to April 14th").\
