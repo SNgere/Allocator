175f68a50fcd84126f3d118f3b553b0d06161a5a
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-07 19:11:37 +0300

    Update job.py

--- job.py
+++ job.py
@@ -1,31 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-# Set page title
-st.set_page_config(page_title="Weekly Work Allocation", page_icon=":memo:", layout="wide")
+# Load the dataframe from CSV file
+df2 = pd.read_csv('path/to/your/csv')
 
-# Set page header
-st.title("Weekly Work Allocation")
-st.markdown("This table shows the weekly work allocation for each team member.")
+# Convert 'Date' column to datetime format
+df2['Date'] = pd.to_datetime(df2['Date'])
 
-# Load the data
-df = pd.read_csv("https://raw.githubusercontent.com/SNgere/Allocator/main/job.csv")
+# Set 'Date' column as the index
+df2.set_index('Date', inplace=True)
 
-# Convert the "Date" column to datetime format
-df['Date'] = pd.to_datetime(df['Date'])
+# Filter the dataframe to only show Monday to Friday
+df2_weekdays = df2.loc[df2.index.weekday < 5]
 
-# Group the data by week and sum the values in each group
-weekly_work = df.groupby(pd.Grouper(key='Date', freq='W-MON')).sum()
-
-# Drop any NaN values in the resulting dataframe
-weekly_work = weekly_work.dropna(how='all')
-
-# Define the start and end date for the date range filter
-start_date = pd.to_datetime('2023-04-10')
-end_date = pd.to_datetime('2023-04-14')
-
-# Filter the data by date range
-filtered_work = weekly_work.loc[start_date:end_date]
-
-# Display the resulting dataframe with formatting
-st.write(filtered_work.style.set_caption("Work allocation from April 10th to April 14th").format("{:.0f}").set_properties(**{'text-align': 'center'}))
+# Display the dataframe in Streamlit
+st.dataframe(df2_weekdays)
