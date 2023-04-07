7f404466367a6d2cbe7467925ec5140c41a572d3
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-07 19:52:21 +0300

    Update job.py

--- job.py
+++ job.py
@@ -21,16 +21,8 @@ def format_date(date):
 # Apply the date format to the index column
 df2_weekdays.index = df2_weekdays.index.map(format_date)
 
-# Add a horizontal line after every Friday
-def add_horizontal_line(row):
-    line = 'border-bottom: 1px solid black'
-    if row.name.endswith('Friday'):
-        return [line]*len(row)
-    else:
-        return ['']*len(row)
+# Draw a horizontal line after every Friday to group data by weeks
+df2_weekdays.loc[df2_weekdays.index.str.startswith('Friday'), :] = '<hr>'
 
-# Apply the horizontal line to the dataframe
-df2_weekdays_styled = df2_weekdays.style.apply(add_horizontal_line, axis=1)
-
-# Display the styled dataframe in Streamlit
-st.write(df2_weekdays_styled)
+# Display the dataframe in Streamlit
+st.write(df2_weekdays, unsafe_allow_html=True)
