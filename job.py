28fbf9c75ed97d7ce641b8b5f1e56d15f921ed3e
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-08 10:43:55 +0300

    Update job.py

--- job.py
+++ job.py
@@ -128,38 +128,35 @@ else:
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv('https://raw.githubusercontent.com/SNgere/Allocator/main/cell_counts.csv')
 
 # Set up plot style
-plt.style.use('seaborn')
-fig, ax = plt.subplots(figsize=(5, 5), facecolor='#f2f2f2')
+plt.style.use('default')
 
 # Create a pie chart with custom colors and explode
 colors = ['#ff6666', '#66b3ff']
 explode = (0.1, 0)
-wedges, texts, autotexts = ax.pie(df['Count'], labels=df['Color'], autopct='%1.1f%%', startangle=90,
-       colors=colors, explode=explode, shadow=True, textprops=dict(color="w"))
-
-# Add arrows inside the pie chart
-center_circle = plt.Circle((0,0),0.70,fc='white')
-fig.gca().add_artist(center_circle)
-
-for i, wedge in enumerate(wedges):
-    ang = (wedge.theta2 - wedge.theta1)/2. + wedge.theta1
-    y = np.sin(np.deg2rad(ang))
-    x = np.cos(np.deg2rad(ang))
-    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-    connectionstyle = f"angle,angleA=0,angleB={ang}"
-    ax.annotate(f"{df['Count'][i]}", xy=(x, y), xytext=(1.2*np.sign(x), 1.2*y),
-            horizontalalignment=horizontalalignment, fontsize=10, fontweight='bold',
-            xycoords=ax.transData, textcoords=ax.transData,
-            arrowprops=dict(arrowstyle="->", color='white', lw=1.5, connectionstyle=connectionstyle))
-
+fig, ax = plt.subplots(figsize=(8, 4))
+wedges, labels, autopct = ax.pie(df['Count'], labels=df['Color'], autopct='%1.1f%%', startangle=90,
+       colors=colors, explode=explode, shadow=True)
 ax.axis('equal')
-ax.set_title('Cell Counts by Fill Color', fontweight='bold', fontsize=16)
+ax.set_title('Cell Counts by Fill Color', fontweight='bold')
+
+# Add arrows to indicate values
+for i, wedge in enumerate(wedges):
+    angle = (wedge.theta2 - wedge.theta1)/2. + wedge.theta1
+    y = wedge.r * 1.1 * np.sin(angle*np.pi/180)
+    x = wedge.r * 1.1 * np.cos(angle*np.pi/180)
+    plt.annotate(f"{df['Count'][i]}",
+                 xy=(0,0), 
+                 xytext=(x,y),
+                 textcoords="offset points",
+                 ha='center',
+                 va='center',
+                 fontsize=10,
+                 fontweight='bold')
 
 # Show the pie chart in Streamlit
 st.pyplot(fig)
