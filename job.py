726ac739c917a9950cfcd30aa847a85ef6172d7f
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-08 10:22:52 +0300

    Update job.py

--- job.py
+++ job.py
@@ -127,35 +127,28 @@ else:
 #####################################################################################################################################
 
 import streamlit as st
-from streamlit_echarts import st_echarts
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv('https://raw.githubusercontent.com/SNgere/Allocator/main/cell_counts.csv')
 
-# Set up chart options
-options = {
-    "title": {"text": "Cell Counts by Fill Color"},
-    "legend": {"data": df["Color"].tolist()},
-    "series": [
-        {
-            "name": "Cell Counts",
-            "type": "pie",
-            "radius": ["40%", "70%"],
-            "avoidLabelOverlap": False,
-            "label": {"show": False, "position": "center"},
-            "emphasis": {"label": {"show": True, "fontSize": "30", "fontWeight": "bold"}},
-            "labelLine": {"show": False},
-            "data": [
-                {"value": df.loc[df["Color"] == "Red", "Count"].iloc[0], "name": "Red"},
-                {"value": df.loc[df["Color"] == "Not Red", "Count"].iloc[0], "name": "Not Red"},
-            ],
-        }
-    ],
-}
+# Set up plot style
+plt.style.use('seaborn')
 
-# Render the chart using Streamlit ECharts
-st_echarts(options=options, height="100px")
+# Create a pie chart with custom colors and explode
+colors = ['#ff6666', '#66b3ff']
+explode = (0.1, 0)
+fig, ax = plt.subplots(figsize=(6,6))
+ax.pie(df['Count'], labels=df['Color'], autopct='%1.1f%%', startangle=90,
+       colors=colors, explode=explode, shadow=True)
+ax.axis('equal')
+ax.set_title('Cell Counts by Fill Color', fontweight='bold')
 
+# Add legend
+legend = ax.legend(loc='best', bbox_to_anchor=(1, 0.5))
+
+# Show the pie chart in Streamlit
+st.pyplot(fig)
 
 
