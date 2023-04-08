5f8703b756ba61a328f3a8264c80230ae03e61d4
SNgere <32767319+SNgere@users.noreply.github.com>
2023-04-08 11:34:43 +0300

    Update job.py

--- job.py
+++ job.py
@@ -139,7 +139,7 @@ plt.style.use('default')
 # Create a pie chart with custom colors and explode
 colors = ['#F8DC75', '#66b3ff']
 explode = (0.05, 0)
-fig, ax = plt.subplots(figsize=(4, 3))
+fig, ax = plt.subplots(figsize=(8, 3))
 wedges, labels = ax.pie(df['Count'], labels=df['Color'], startangle=180,
        colors=colors, explode=explode, shadow= False)
 ax.axis('equal')
